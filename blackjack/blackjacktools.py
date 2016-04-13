673ac6851309d68e553ed91b5b04eb5d2d59c58d
Ryan Heilman <rhheilma@asu.edu>
2016-04-13 14:19:46 -0700

    Added players and dealers, created score

--- blackjack/blackjacktools.py
+++ blackjack/blackjacktools.py
@@ -20,6 +20,13 @@ class Card(object):
 class Deck(object):
     def __init__(self):
         self.cards = []
+    
+    def shuffle(self,n=3):
+        self.cards = []
+        for _ in range(n):
+            for x in list(range(2,11)) + [10,10,10,"Ace"]:
+                for y in ["Clubs","Spades","Hearts","Diamonds"]:
+                    self.cards.append(Card(x,y))
 
     def add_card(self,cards):
         if type(cards) is list:
@@ -30,7 +37,7 @@ class Deck(object):
     def deal(self):
         if self.__len__() == 0:
             print("Deck is empty!")
-            return card(None,None)
+            return Card(None,None)
         return self.cards.pop(random.randrange(0,len(self.cards)))
 
     def summary(self):
@@ -42,4 +49,33 @@ class Deck(object):
     def __len__(self):
         return len(self.cards)
     
+class Agent(object):
+    def __init__(self):
+        self.hand = []
+    def add_card(self,card):
+        self.hand.append(card)
+    def clear_hand(self):
+        self.hand = []
+    def get_score(self):
+        score = 0
+        aces = []
+        for c in self.hand:
+            if type(c.rank) is int:
+                score += c.rank
+            else:
+                aces.append(c)
+        for a in aces:
+            if (score + 11) > 21:
+                score += 1
+            else:
+                score += 11
+        return score 
+        
+class Player(Agent):
+    def __init__(self):
+        super(Player,self).__init__()
+        
+class Dealer(Agent):
+    def __init__(self):
+        super(Dealer,self).__init__()
     
