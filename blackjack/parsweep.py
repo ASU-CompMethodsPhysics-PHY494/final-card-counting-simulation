f9fc6a1a9c68e7cffb142f96eae8553a8a11059c
Layne Bradshaw <lnbradsh@asu.edu>
2016-05-02 15:32:13 -0700

    Test plots v2. w/Ryan

--- blackjack/parsweep.py
+++ blackjack/parsweep.py
@@ -11,16 +11,17 @@ import matplotlib.pyplot as plt
 # In[10]:
 
 M = 100
-Hmax = 105
-Lmin = 95
-for H in range(M,Hmax):
-    for L in range(Lmin,M):
+Hmax = 150
+Lmin = 50
+for H in range(M,Hmax,5):
+    for L in range(Lmin,M,5):
         table = bj.Table()
         table.shuffle_deck(n=6)
         table.set_players([bj.Player(L,H,bank_roll = 5000) for _ in range(5)])
-        bal = table.run(max_hands = 10)
+        bal = table.run(max_hands = 800)
         plt.title("{} {}".format(L,H))
         plt.plot(bal.T)
+        plt.savefig("img/{}_{}.pdf".format(L,H))
         plt.show()
 
 
